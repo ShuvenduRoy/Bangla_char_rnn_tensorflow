e0ebd6f32f9b8c2010df2516527c370a71b2fc43
Shuvendu Bikash <bikash11roy@gmail.com>
2017-11-06 06:50:44 +0600

    bangla char rnn
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

---
+++ sample.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+import tensorflow as tf
+
+import argparse
+import os
+from six.moves import cPickle
+
+from model import Model
+
+from six import text_type
+
+
+def main():
+    parser = argparse.ArgumentParser(
+                       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--save_dir', type=str, default='save',
+                        help='model directory to store checkpointed models')
+    parser.add_argument('-n', type=int, default=500,
+                        help='number of characters to sample')
+    parser.add_argument('--prime', type=text_type, default='একটি',
+                        help='prime text')
+    parser.add_argument('--sample', type=int, default=1,
+                        help='0 to use max at each timestep, 1 to sample at '
+                             'each timestep, 2 to sample on spaces')
+
+    args = parser.parse_args()
+    sample(args)
+
+
+def sample(args):
+    with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
+        saved_args = cPickle.load(f)
+    with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'rb') as f:
+        chars, vocab = cPickle.load(f)
+    model = Model(saved_args, training=False)
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        saver = tf.train.Saver(tf.global_variables())
+        ckpt = tf.train.get_checkpoint_state(args.save_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            result = (model.sample(sess, chars, vocab, args.n, args.prime, args.sample).encode('utf-8'))
+            result = result.decode("utf-8", "replace")
+            #
+            # result = str((model.sample(sess, chars, vocab, 500, current_sentence, 1).encode('utf-8')))
+            print(result)
+            print(type(result))
+
+            #word = result.split("\n")[0].split(" ")[len(args.split(" ")) - 1]
+            print(result.split(" ")[(len(args.prime.split(" ")))])
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ word_predict.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+
+prime = sys.argv[1]
+result = os.popen('python sample.py --save_dir save --prime "' + prime  + '"').read()
+print (result.split("\n")[0].split(" ")[len(prime.split(" "))-1])
